91903389671cd50b0f193782fe00a17a974ecff4
kitbeck <mail@kitbeck.co.uk>
2012-11-10 17:37:19 +0000

    added knobs.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+$(".dial_green").knob({
+	"min":0,
+	"max":100,
+	"fgColor":"#00ff00"
+});
+
+$(".dial_orange").knob({
+	"min": 0,
+	"max": 100,
+	"fgColor": "#f30"
+});
 
 
 });
